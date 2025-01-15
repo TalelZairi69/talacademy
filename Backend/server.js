0eadfd81436c3a6d07b2c32454a24e42e49ea644
Talel Zairi <118089503+TalelZairi69@users.noreply.github.com>
2025-01-15 16:25:42 +0300

    qsqsd

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: ['https://talacademy.onrender.com', 'https://talacademy.onrender.com', 'https://talacademy.onrender.com'],
+    origin: 'https://talacademy.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
 }));
